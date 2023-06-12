38d41abcae3dca58446893005d709118f4aa0486
IdoKedem <idokedem12@gmail.com>
2023-06-13 01:22:28 +0300

    removed comments

--- Server.py
+++ Server.py
@@ -19,20 +19,3 @@ def server_accept():
 def server_recive():
     msg = client.recv(1024)
     print(msg.decode())
-
-if __name__ == '__main__':
-    # PORT = 4444
-    # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #
-    # server_socket.bind(('192.168.14.20', PORT))
-    #
-    # server_socket.listen()
-    # print("Server Waiting...")
-    #
-    # client, ip = server_socket.accept()
-    # print("Server Connected!")
-    #
-    # a = client.recv(1024).decode()
-    # print(a)
-
-    pass
